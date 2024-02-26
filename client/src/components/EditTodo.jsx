5b37211c600ed41f057bec4a557d4cdc9fab2792
Fabio Fernandes <fabiofernandes@fabios-mbp.home>
2024-02-26 17:21:44 +0000

    Fixed linting issues

--- client/src/components/EditTodo.jsx
+++ client/src/components/EditTodo.jsx
@@ -9,7 +9,7 @@ const EditTodo = ({ todo }) => {
     e.preventDefault();
     try {
       const body = { description };
-      const response = await fetch(
+      await fetch(
         `http://localhost:5000/todos/${todo.todo_id}`,
         {
           method: "PUT",
